b94f0d6ac679e245adeb10481c16114dcb9c9fda
Aelita <xsjctony@126.com>
2023-11-28 22:11:00 +1100

    ci: add config for `changelogithub`

---
+++ changelogithub.config.ts
@@ -0,0 +1,19 @@
+import type { ChangelogOptions } from 'changelogithub'
+
+
+export default {
+  types: {
+    feat: { title: '🚀 Features' },
+    fix: { title: '🐞 Bug Fixes' },
+    perf: { title: '🔥 Performance' },
+    refactor: { title: '💅 Refactors' },
+    docs: { title: '📖 Documentation' },
+    build: { title: '📦 Build' },
+    types: { title: '🌊 Types' },
+    chore: { title: '🏡 Chore' },
+    examples: { title: '🏀 Examples' },
+    test: { title: '🧪 Tests' },
+    style: { title: '🎨 Styles' },
+    ci: { title: '🤖 CI' }
+  }
+} satisfies ChangelogOptions
