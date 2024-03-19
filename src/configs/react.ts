e7debb657924a64b9f3cc97da904c42f376c605f
Tony Jiang <tjiang@nowfinance.com.au>
2024-03-19 15:57:10 +1100

    fix(react): fix `jsx-boolean-value` error

--- src/configs/react.ts
+++ src/configs/react.ts
@@ -89,7 +89,7 @@ const reactRules = (
 
 const jsxRules = (ruleOptions: NonNullable<ReactOptions['ruleOptions']>): FlatConfigItem['rules'] => ({
   // @ts-expect-error - type definition is not up-to-date
-  'react/jsx-boolean-value': ['error', 'never', [{ assumeUndefinedIsFalse: false }]],
+  'react/jsx-boolean-value': ['error', 'never', { assumeUndefinedIsFalse: false }],
   'react/jsx-filename-extension': ['error', { extensions: ['.jsx'] }],
   'react/jsx-fragments': ['error', 'syntax'],
   'react/jsx-key': [
