f29ff3a8b6d3070738d2666efe4ec204459d0287
Aelita <xsjctony@126.com>
2024-03-23 19:14:22 +1100

    fix: dynamic require of "tty" is not supported

---
+++ cjs-shim.ts
@@ -0,0 +1,8 @@
+import { createRequire } from 'node:module'
+import { dirname } from 'node:path'
+import { fileURLToPath } from 'node:url'
+
+
+globalThis.require = createRequire(import.meta.url)
+globalThis.__filename = fileURLToPath(import.meta.url)
+globalThis.__dirname = dirname(__filename)
--- tsup.config.ts
+++ tsup.config.ts
@@ -5,6 +5,7 @@ export default defineConfig({
   entry: [
     'src/index.ts',
   ],
+  inject: ['cjs-shim.ts'],
   external: ['@next/eslint-plugin-next'],
   shims: true,
   format: ['cjs', 'esm'],
