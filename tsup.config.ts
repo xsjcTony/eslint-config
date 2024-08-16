3cd02c2435292bb70136f409d22a52f11505fdf1
Aelita <xsjctony@126.com>
2024-08-17 01:20:29 +1000

    build: remove unnecessary `external`

--- tsup.config.ts
+++ tsup.config.ts
@@ -6,7 +6,6 @@ export default defineConfig({
     'src/index.ts',
   ],
   inject: ['cjs-shim.ts'],
-  external: ['@next/eslint-plugin-next'],
   shims: true,
   format: ['cjs', 'esm'],
   clean: true,
