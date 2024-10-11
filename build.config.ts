7dfc4e27ac324869e410a42a72feaa013f5ef452
Tony Jiang <tjiang@nowfinance.com.au>
2024-10-11 11:50:11 +1100

    fix(cjs): use `unbuild` instead of `tsup` to support CJS usage

---
+++ build.config.ts
@@ -0,0 +1,17 @@
+import { defineBuildConfig } from 'unbuild'
+
+
+export default defineBuildConfig({
+  entries: [
+    'src/index',
+  ],
+  clean: true,
+  declaration: true,
+  externals: ['@typescript-eslint/types'],
+  rollup: {
+    emitCJS: true,
+    dts: {
+      respectExternal: false,
+    },
+  },
+})
--- cjs-shim.ts
+++
@@ -1,8 +0,0 @@
-import { createRequire } from 'node:module'
-import { dirname } from 'node:path'
-import { fileURLToPath } from 'node:url'
-
-
-globalThis.require = createRequire(import.meta.url)
-globalThis.__filename = fileURLToPath(import.meta.url)
-globalThis.__dirname = dirname(globalThis.__filename)
